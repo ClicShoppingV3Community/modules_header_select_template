4438c98c1dc64aeb05e553749206ca357192926f
ClicShopping - e-commerce Solution B2B B2C <39338579+ClicShopping@users.noreply.github.com>
2019-12-30 14:20:20 -0500

    small update

--- sources/template/Default/modules/modules_header/he_header_select_template.php
+++ sources/template/Default/modules/modules_header/he_header_select_template.php
@@ -80,10 +80,10 @@
       $CLICSHOPPING_Db = Registry::get('Db');
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Souhaitez-vous activer ce module ?',
+          'configuration_title' => 'Do you want to enable this module ?',
           'configuration_key' => 'MODULE_HEADER_SELECT_TEMPLATE_STATUS',
           'configuration_value' => 'True',
-          'configuration_description' => 'Souhaitez vous activer ce module à votre boutique ?',
+          'configuration_description' => 'Do you want to enable this module in your shop ?',
           'configuration_group_id' => '6',
           'sort_order' => '1',
           'set_function' => 'clic_cfg_set_boolean_value(array(\'True\', \'False\'))',
@@ -92,10 +92,10 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Veuillez indiquer la largeur du contenu',
+          'configuration_title' => 'Please indicate the width of the content',
           'configuration_key' => 'MODULE_HEADER_SELECT_TEMPLATE_CONTENT_WIDTH',
           'configuration_value' => '12',
-          'configuration_description' => 'Veuillez indiquer une largeur d\'affichage',
+          'configuration_description' => 'Please specify a display width',
           'configuration_group_id' => '6',
           'sort_order' => '2',
           'set_function' => 'clic_cfg_set_content_module_width_pull_down',
@@ -104,10 +104,10 @@
       );
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Ordre de tri d\'affichage',
+          'configuration_title' => 'Sort order',
           'configuration_key' => 'MODULE_HEADER_SELECT_TEMPLATE_SORT_ORDER',
           'configuration_value' => '1',
-          'configuration_description' => 'Ordre de tri pour l\'affichage (Le plus petit nombre est montré en premier)',
+          'configuration_description' => 'Sort order of display. Lowest is displayed first. The sort order must be different on every module',
           'configuration_group_id' => '6',
           'sort_order' => '3',
           'set_function' => '',
@@ -117,7 +117,7 @@
 
 
       $CLICSHOPPING_Db->save('configuration', [
-          'configuration_title' => 'Veuillez indiquer ou la boxe doit s\'afficher',
+          'configuration_title' => 'Please indicate where boxing should be displayed',
           'configuration_key' => 'MODULE_HEADER_SELECT_TEMPLATE_DISPLAY_PAGES',
           'configuration_value' => 'all',
           'configuration_description' => 'Sélectionnez les pages o&ugrave; la boxe doit être présente',
